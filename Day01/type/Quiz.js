e9f0c4fb4f4c305d19c223f1ae81ebaaccf4c833
rlaehddud475 <6070kjh475@naver.com>
2024-09-19 17:14:35 +0900

    20240919

---
+++ Day01/type/Quiz.js
@@ -0,0 +1,4 @@
+const a = prompt("첫 번째 숫자 입력");
+const b = prompt("두 번째 숫자 입력");
+const result = Number(a) + Number(b);
+console.log(`두 수의 합은 ${result}입니다.`);
---
+++ Day01/type/index.js
@@ -0,0 +1,25 @@
+//문자열(String)
+// const num = "1";
+// const num1 = `1`;
+// const num2 = "1";
+
+//숫자(number/int/integer)
+// const age = 100;
+// const age1 = 20;
+
+// console.log(age + age1);
+// console.log(num + num1 + num2);
+
+// number,string 타입 바꾸기
+// number화 시키기
+const a = Number("100");
+const b = Number("200");
+console.log(a + b);
+const c = Number("10");
+const d = Number("50");
+console.log(c + d);
+
+//String화 시키기
+const e = String(100);
+const f = String(4);
+console.log(e + f);
