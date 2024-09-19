4ab40644dae6fd538263139c0139d4f7de868034
rlaehddud475 <6070kjh475@naver.com>
2024-09-19 16:53:52 +0900

    20240919

---
+++ Day01/prompt/index.js
@@ -0,0 +1,10 @@
+// const age = prompt("님 연세가 어떻게 되심?");
+// console.log(`연세가 ${age} 이시니 딸피시네요`);
+
+const food = prompt("추석에 먹은 음식은");
+const doing = prompt("추석에 뭐했음?");
+const mood = prompt("오늘 기분 어떠심?");
+
+console.log(
+  `추석에 먹은 음식은 ${food} 이고, ${doing}을 하셨고, 오늘기분이 ${mood} 하시군요! 그래도 화이팅임 ㅅㄱ`
+);
---
+++ Day01/variables/index.js
@@ -0,0 +1,20 @@
+// const lunch = "볶음 짬뽕";
+// alert(lunch);
+
+// const coffee = "아이스 아메리카노";
+// console.log(coffee);
+
+// const mbti = "infp";
+// console.log(mbti);
+
+// console.log(`내 mbti는 ${mbti}`);
+// console.log("내 mbti는 " + mbti);
+
+const name = "김동영";
+const mbti = "infp";
+const ott = "왓챠";
+const youtube = "만당";
+const dinner = "모름";
+
+console.log(`안녕하세요 저의 이름은 ${name} 입니다. 저의 mbti는 ${mbti} 이고요, 제가 좋아하는 OTT플렛 폼은 ${ott} 입니다.
+요즘 즐겨 보는 유튜버는 ${youtube}입니다. 오늘저녁은 ${dinner} 먹을 예정입니다.`);
