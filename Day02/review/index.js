86b3ed037159b9ba5d09910345b89901e16dbd7b
rlaehddud475 <6070kjh475@naver.com>
2024-09-20 09:41:52 +0900

    20240920

---
+++ Day02/review/index.js
@@ -0,0 +1,11 @@
+const a = prompt("운동 순서를 입력하세요");
+const b = prompt("운동 순서를 입력하세요");
+const c = prompt("운동 순서를 입력하세요");
+console.log(`운동 순서 : ${a}->${b}->${c}`);
+
+const price = Number(prompt("커피 한 잔 가격은?"));
+const quantity = Number(prompt("커피의 수량은?"));
+console.log(`총 가격은 ${price * quantity}입니다.`);
+
+const year = Number(prompt("태어난 연도는?"));
+console.log(`${2024 - year}살 이군요`);
