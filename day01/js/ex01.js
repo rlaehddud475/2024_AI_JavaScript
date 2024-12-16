160a4324b400637b62b225445ef011c7d7f79bca
rlaehddud475 <6070kjh475@naver.com>
2024-12-16 11:20:41 +0900

    20241216

---
+++ day01/js/ex01.js
@@ -0,0 +1,9 @@
+
+//const buttonEl = document.getElementById("button");
+//console.log("buttonEl", buttonEl);
+
+window.addEventListener("DOMContentLoaded", function () {
+    const buttonEl = document.getElementById("button");
+    console.log("buttonEl", buttonEl);
+  });
+  
